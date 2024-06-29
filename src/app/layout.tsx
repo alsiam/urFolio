69b90848c44409c5812af377a24d283d3893d7f4
GScriptS <simoneguarnuccio@gmail.com>
2024-06-29 20:33:29 +0200

    feat: update meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ import ThemeSwitch from "@/components/panel/ThemeSwitch";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "gscripts - Portfolio",
-  description: "",
+  title: "GS Developer",
+  description: "Presentational portfolio",
 };
 
 export default function RootLayout({
