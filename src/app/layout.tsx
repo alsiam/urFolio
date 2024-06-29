cfed5cc73fd9d291e2a75e5360a377f6286e72cd
GScriptS <simoneguarnuccio@gmail.com>
2024-06-29 12:06:47 +0200

    feat: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ import ThemeSwitch from "@/components/panel/ThemeSwitch";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "urFolio - Portfolio web",
-  description: "urFolio - a Open Source portfolio web application",
+  title: "gscripts - Portfolio",
+  description: "",
 };
 
 export default function RootLayout({
