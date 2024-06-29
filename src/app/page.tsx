3e7ed37b254d2014940cdca8261e18481a1f3db6
GScriptS <simoneguarnuccio@gmail.com>
2024-06-29 14:16:39 +0200

    feat: updat url socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,13 +22,13 @@ export default async function Home() {
               <a href="https://facebook.com/" target="_blank">
                 <i className="fa fa-facebook" />
               </a>
-              <a href="https://github.com/" target="_blank">
+              <a href="https://github.com/grnsmn" target="_blank">
                 <i className="fa fa-github" />
               </a>
               <a href="https://instagram.com/" target="_blank">
                 <i className="fa fa-instagram" />
               </a>
-              <a href="https://linkedin.com/in/" target="_blank">
+              <a href="https://www.linkedin.com/authwall?trk=gf&trkInfo=AQGx5HKBKsr_TQAAAZBj64m4B-v-SF6jUFHCW1zj2Jp0chYKE-nSMUpwmBs-U1YKTNRb7Zgx6Z_okklU2MrdF3nuimoWHZjELM9rI5HSDfVh1yQShbYYsWPUO3vKApgv6-fa5Y4=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fsimone-guarnuccio-7779a4166%3Futm_source%3Dshare%26utm_campaign%3Dshare_via%26utm_content%3Dprofile%26utm_medium%3Dandroid_app" target="_blank">
                 <i className="fa fa-linkedin" />
               </a>
             </div>
