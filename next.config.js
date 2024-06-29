f7965ae0413782066983d740c1fbbeecbc91e239
GScriptS <simoneguarnuccio@gmail.com>
2024-06-29 12:57:27 +0200

    feat: hide unoptimizing images in config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        unoptimized: true,
-    }
+    // images: {
+    //     unoptimized: true,
+    // }
 }
 
 module.exports = nextConfig
