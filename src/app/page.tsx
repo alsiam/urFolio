5e051deeb7d8512393159c242766a5469073ed93
GScriptS <simoneguarnuccio@gmail.com>
2024-06-29 23:43:13 +0200

    feat: update link to instagram and facebook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,13 +19,13 @@ export default async function Home() {
               {/* <a href="https://twitter.com/" target="_blank">
                 <i className="fa fa-twitter" />
               </a> */}
-              <a href="https://facebook.com/" target="_blank">
+              <a href="https://www.facebook.com/simone.guarnuccio" target="_blank">
                 <i className="fa fa-facebook" />
               </a>
               <a href="https://github.com/grnsmn" target="_blank">
                 <i className="fa fa-github" />
               </a>
-              <a href="https://instagram.com/" target="_blank">
+              <a href="https://www.instagram.com/simoneguarnuccio" target="_blank">
                 <i className="fa fa-instagram" />
               </a>
               <a href="https://www.linkedin.com/authwall?trk=gf&trkInfo=AQGx5HKBKsr_TQAAAZBj64m4B-v-SF6jUFHCW1zj2Jp0chYKE-nSMUpwmBs-U1YKTNRb7Zgx6Z_okklU2MrdF3nuimoWHZjELM9rI5HSDfVh1yQShbYYsWPUO3vKApgv6-fa5Y4=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fsimone-guarnuccio-7779a4166%3Futm_source%3Dshare%26utm_campaign%3Dshare_via%26utm_content%3Dprofile%26utm_medium%3Dandroid_app" target="_blank">
